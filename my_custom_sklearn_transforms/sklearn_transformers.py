021aa5a8e5340e2f0ff20ae36157f22a94e52e47
rodolphorss <69733242+rodolphorss@users.noreply.github.com>
2020-08-21 22:02:04 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn import SimpleImputer
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -10,7 +10,19 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        data_2 =  data.drop(labels=self.columns, axis='columns')
+
+        si = SimpleImputer(
+            missing_values=np.nan,
+            strategy='median'
+        )
+        si.fit(data_2.select_dtypes(exclude=['object']))
+
+        # Reconstrução de um novo DataFrame Pandas com o conjunto imputado
+        data3 = data_2.select_dtypes(exclude=['object'])
+        data3 = si.transform(data_2.select_dtypes(exclude=['object']))
+        data4 = pd.DataFrame.from_records(data3, columns=data_2.columns[data_2.columns != 'PERFIL'])
+        data4['PERFIL'] = X['PERFIL']
+        return data4
+
